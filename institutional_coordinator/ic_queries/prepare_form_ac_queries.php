289c55002746cb0dc331d0ecf878be8a64e66711
dc480506 <iamdev.dc@gmail.com>
2020-03-09 17:01:36 +0530

    Prepare Form Completed for Institute Coordinator

---
+++ institutional_coordinator/ic_queries/prepare_form_ac_queries.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+if(isset($_SESSION['email']) && $_SESSION['role']=='inst_coor'){
+    include_once('../../config.php');
+    // echo "Hi";
+    if(isset($_POST['createForm'])){
+        $nop=mysqli_escape_string($conn,$_POST['nop']);
+        $sem=mysqli_escape_string($conn,$_POST['sem']);
+        $year=mysqli_escape_string($conn,$_POST['year']);
+        $start_date=mysqli_escape_string($conn,$_POST['start_date']);
+        $start_time=mysqli_escape_string($conn,$_POST['start_time']);
+        $end_date=mysqli_escape_string($conn,$_POST['end_date']);
+        $end_time=mysqli_escape_string($conn,$_POST['end_time']);
+        date_default_timezone_set('Asia/Kolkata');
+        $timestamp=date("Y-m-d H:i:s");
+        $email=$_SESSION['email'];
+        $sql="INSERT INTO audit_form (`sem`,`year`,`email_id`,`timestamp_created`,`start_date`,`end_date`,`start_time`,
+        `end_time`,`no_of_preferences`) VALUES('$sem','$year','$email','$timestamp','$start_date','$end_date','$start_time','$end_time','$nop')";
+        mysqli_query($conn,$sql) or die(mysqli_error($conn));
+        header("Location: ../prepare_form_ac.php");
+    }else if(isset($_POST['deleteForm'])){
+        $sem=mysqli_escape_string($conn,$_POST['sem']);
+        $year=mysqli_escape_string($conn,$_POST['year']);
+        $sql="DELETE FROM audit_form WHERE sem='$sem' AND year='$year'";
+        mysqli_query($conn,$sql) or die(mysqli_error($conn));
+        header("Location: ../prepare_form_ac.php");
+    }else if(isset($_POST['modifyForm'])){
+        $nop=mysqli_escape_string($conn,$_POST['nop']);
+        $sem=mysqli_escape_string($conn,$_POST['sem']);
+        $year=mysqli_escape_string($conn,$_POST['year']);
+        $oldsem=mysqli_escape_string($conn,$_POST['oldsem']);
+        $oldyear=mysqli_escape_string($conn,$_POST['oldyear']);
+        $start_date=mysqli_escape_string($conn,$_POST['start_date']);
+        $start_time=mysqli_escape_string($conn,$_POST['start_time']);
+        $end_date=mysqli_escape_string($conn,$_POST['end_date']);
+        $end_time=mysqli_escape_string($conn,$_POST['end_time']);
+        $sql="UPDATE audit_form SET no_of_preferences='$nop',sem='$sem',year='$year',start_date='$start_date',end_date='$end_date'
+        ,start_time='$start_time',end_time='$end_time' WHERE sem='$oldsem' AND year='$year'";
+        mysqli_query($conn,$sql) or die(mysqli_error($conn));
+        header("Location: ../prepare_form_ac.php");
+    }
+}
+
+?>
