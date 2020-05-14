2ef00ff0216f383bf88cc76a30c12556165feefa
hgokhru101 <hgokhru101@gmail.com>
2020-05-14 12:48:19 +0530

    Upload Response GUI updated

--- AC_EC (editable)/institutional_coordinator/addstudent.php
+++ AC_EC (editable)/institutional_coordinator/addstudent.php
@@ -411,50 +411,7 @@ include('../includes/header.php');
             </div>
         </div>
     </div>
-    <style type="text/css">
-        @media only screen and (max-width: 760px),
-        (min-device-width: 768px) and (max-device-width: 1024px) {
-
-            /* Force table to not be like tables anymore */
-            table,
-            thead,
-            tbody,
-            th,
-            td,
-            tr {
-                display: block;
-            }
-
-            /* Hide table headers (but not display: none;, for accessibility) */
-            thead tr {
-                position: absolute;
-                top: -9999px;
-                left: -9999px;
-            }
-
-            tr {
-                border: 1px solid #ccc;
-            }
-
-            td {
-                /* Behave  like a "row" */
-                border: none;
-                border-bottom: 1px solid #eee;
-                position: relative;
-                padding-left: 50%;
-            }
-
-            td:before {
-                /* Now like a table header */
-                position: absolute;
-                /* Top/left values mimic padding */
-                top: 6px;
-                left: 6px;
-                width: 45%;
-                padding-right: 10px;
-                white-space: nowrap;
-            }
-    </style>
+    
     <!-- /.container-fluid -->
     <?php include('../includes/footer.php');
     include('../includes/scripts.php');
