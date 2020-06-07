2fae132591f0c7abbb1323aa605fcbc970f67a32
krunal-188 <krunaldabhi92@gmail.com>
2020-06-07 05:46:42 +0530

    added course analysis and fixed some bugs in course selection

--- AC_EC (editable)/institutional_coordinator/allocation/loadTabs/load_course_selection_tab.php
+++ AC_EC (editable)/institutional_coordinator/allocation/loadTabs/load_course_selection_tab.php
@@ -9,9 +9,11 @@
  $timestamp=date("Y-m-d H:i:s");
  $_SESSION['course_table']=$hash."_".$_SESSION['type']."_course";
 //  echo $_SESSION['course_table'];
+ $_SESSION['student_pref']=$hash."_student_pref_".$_SESSION['type'];
  $_SESSION['student_course_table']=$hash."_student_".$_SESSION['type'];
+ $_SESSION['course_allocate_info']=$hash."_".$_SESSION['type']."_course_info";
  mysqli_query($conn,"INSERT INTO delete_temp_tables VALUES ('".$_SESSION['course_table']."','".$timestamp."'),
-        ('".$_SESSION['student_course_table']."','".$timestamp."')");
+        ('".$_SESSION['student_pref']."','".$timestamp."'),('".$_SESSION['student_course_table']."','".$timestamp."'),('".$_SESSION['course_allocate_info']."','".$timestamp."')");
  $result=mysqli_query($conn,'CREATE TABLE '.$_SESSION['course_table'].' (
     `cid` varchar(30) NOT NULL,
     `sem` int(11) NOT NULL,
@@ -27,10 +29,16 @@
   )');
 //   echo $result." yo";
   $preferences="";
+  $pref="";
   for($i=1;$i<=$_SESSION['no_of_preferences'];$i++){
       $preferences.='`pref'.$i.'` varchar(15) NOT NULL DEFAULT(""),';
-  }
-  mysqli_query($conn,'CREATE TABLE '.$_SESSION['student_course_table'].' (
+      if($i!=$_SESSION['no_of_preferences'])
+      {
+        $pref.='`pref'.$i.'`,';
+      }
+    }
+  $pref.='`pref'.$_SESSION['no_of_preferences'].'`';
+  mysqli_query($conn,'CREATE TABLE '.$_SESSION['student_pref'].' (
     `email_id` varchar(50) NOT NULL,
     `sem` int(11) NOT NULL,
     `year` varchar(8) NOT NULL,
@@ -46,9 +54,25 @@
   SELECT cid,sem,year,cname,currently_active,min,max,no_of_allocated,email_id,timestamp 
   FROM '.$_SESSION['type'].'_course WHERE sem="'.$_SESSION['sem'].'" AND year="'.$_SESSION['year'].'"
   ');
+  mysqli_query($conn,'CREATE TABLE '.$_SESSION['student_course_table'].'(
+    `email_id` varchar(50) NOT NULL,
+    `cid` varchar(30) NOT NULL,
+    `sem` int(11) NOT NULL,
+    `year` varchar(8) NOT NULL,
+    PRIMARY KEY (email_id,sem,year)
+)');
+  mysqli_query($conn,'CREATE TABLE '.$_SESSION['course_allocate_info'].'(
+    `cid` varchar(30) NOT NULL,
+    `sem` int(11) NOT NULL,
+    `year` varchar(8) NOT NULL,
+    `status` varchar(2) NOT NULL,
+    `no_of_hits` int(11) NOT NULL,
+    PRIMARY KEY(cid,sem,year)
+)');
+  mysqli_query($conn,'INSERT INTO '.$_SESSION['student_pref'].'(`email_id`,`sem`,`year`,`rollno`,`timestamp`,`allocate_status`,`no_of_valid_preferences`,'.$pref.') SELECT email_id,sem,year,rollno,timestamp,allocate_status,no_of_valid_preferences,'.$pref.' FROM student_preference_'.$_SESSION['type'].' WHERE sem="'.$_SESSION['sem'].'" AND year="'.$_SESSION['year'].'"');
 ?>
 <div class="tab-pane fade show active" id="nav-course" role="tabpanel" aria-labelledby="nav-course-tab">
-     <form class="forms-sample" method="POST" id='course_selection'>
+     <form class="forms-sample" method="POST" id="course_selection">
     <table class="table table-bordered table-responsive" id="dataTable-course" width="100%" cellspacing="0">
                             <thead>
                                 <tr>
@@ -80,12 +104,22 @@
                                 </tr>
                             </tfoot>
                         </table>
-                    </form>
-</div>
-<div class="modal-footer">
-    <button type="submit" class="btn btn-secondary align-center" name="previous">Previous</button>
-    <button type="submit" class="btn btn-primary align-center" name="allocate">Next</button>
-</div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-secondary align-center" name="previous">Previous</button>
+                        <button type="submit" class="btn btn-primary align-center" name="allocate">Next</button>
+                    </div>
+                </form>
+            </div>
+            <div id="spinner" style="display: none;">
+                <img src="loadTabs/ajax-loader.gif" alt="loading" id="img-spinner">
+            </div>
+            <style type="text/css">
+                #spinner{
+                    position: fixed;
+                    top: 50%;
+                    left:50%;
+                }
+            </style>
 <script type="text/javascript">
    $(document).ready(function() {
         loadCurrent();
@@ -98,6 +132,7 @@
             destroy: true,
             serverMethod: 'post',
             aaSorting: [],
+            pageLength:50,
             ajax: {
                 'url': '../allocation/loadInfo/select_course.php'
             },
@@ -183,10 +218,17 @@
     // var btn=$(this);
     var aPos = $("#dataTable-course").dataTable().fnGetPosition(target_row.get(0)); 
     var courseData=$('#dataTable-course').DataTable().row(aPos).data()
+    console.log(courseData)
+    course_post_data={}
+    course_post_data['cid']=courseData.cid.split(">")[1].split("</")[0].trim()
+    course_post_data['max']=courseData.max.split(">")[1].split("</")[0].trim()
+    course_post_data['min']=courseData.min.split(">")[1].split("</")[0].trim()
+    course_post_data['cname']=courseData.cname.split(">")[1].split("</")[0].trim()
+        
     // delete courseData.action
-    // console.log(courseData)
+    console.log(course_post_data)
     // delete courseData.allocate_faculty
-    var json_courseData=JSON.stringify(courseData)
+    var json_courseData=JSON.stringify(course_post_data)
     $.ajax({
         type: "POST",
         url: "../allocation/loadModal/select_course_modal.php",
@@ -255,18 +297,55 @@
         var row=$("#update-del-modal").closest('tr');
         var aPos = $("#dataTable-course").dataTable().fnGetPosition(row.get(0));
         var temp = $("#dataTable-course").DataTable().row(aPos).data();
-        //console.log(temp)
+        console.log(temp)
         //console.log(form_serialize)
-        temp['cid'] = form_serialize[1].value;
-        temp['min'] = form_serialize[2].value;    //new values
-        temp['max'] = form_serialize[3].value;    //new values
+        var color="text-success";
+        var maximum=form_serialize[3].value
+        var fc=temp.firstcount.split(">")[1].split("</")[0].trim()
+        var fp=temp.firstpercent.split(">")[1].split("</")[0].trim()
+        var name=temp.cname.split(">")[1].split("</")[0].trim()
+        //console.log('max: '+maximum+' fc: '+fc)
+        if(parseInt(maximum)<parseInt(fc))
+           { color="text-danger";}
+        //console.log(color)
+        temp['cid'] = '<span class="'+color+'">'+form_serialize[1].value+'</span>';
+        temp['min'] = '<span class="'+color+'">'+form_serialize[2].value+'</span>';    //new values
+        temp['max'] = '<span class="'+color+'">'+form_serialize[3].value+'</span>';
+        temp['cname']= '<span class="'+color+'">'+name+'</span>';   //new values
+        temp['firstcount']='<span class="'+color+'">'+fc+'</span>';
+        temp['firstpercent']='<span class="'+color+'">'+fp+'</span>';
         //console.log(temp)
         $('#dataTable-course').dataTable().fnUpdate(temp,aPos,undefined,false);
         $('.action-btn').off('click')
         $('.action-btn').on('click',loadModalCurrent)
         // $("#dataTable-student").DataTable().row(aPos).draw(false);
-        $(".selectrow_course").attr("disabled",true);
+        //$(".selectrow_course").attr("disabled",true);
     }
     });
 }
+$("#course_selection").submit(function(e){
+        e.preventDefault();
+        var form=$(this);
+        form_serialize=form.serializeArray();
+        console.log(form_serialize);
+        $("#nav-course-tab").removeClass("active")
+        $("#nav-course-tab").addClass("disabled")
+        $.ajax({
+            type:"POST",
+            url:"loadTabs/load_allocation_analysis_tab.php",
+            data:form_serialize,
+            beforeSend:function(){
+            //Loader daalna hai baadme
+            $('#spinner').show;
+            },
+            success:function(html){
+                $("#nav-result-tab").removeClass("disabled")
+                $("#nav-tabContent").html(html)
+                $("#nav-result-tab").addClass("active")
+            },
+            complete:function(){
+                $('#spinner').hide;
+            }
+        })
+    })
 </script>
