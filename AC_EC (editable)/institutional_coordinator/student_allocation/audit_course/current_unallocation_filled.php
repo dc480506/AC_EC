9ad1161a037acdc636f76f9d776c8f5540e6a984
meetb007 <meet.rb@somaiya.edu>
2020-06-21 19:17:15 +0530

    student allocation done

--- AC_EC (editable)/institutional_coordinator/student_allocation/audit_course/current_unallocation_filled.php
+++ AC_EC (editable)/institutional_coordinator/student_allocation/audit_course/current_unallocation_filled.php
@@ -12,7 +12,7 @@ if(isset($_POST['order'])){
 }else{
    // $columnName='sem,year';
    // $columnSortOrder='desc';
-   $orderQuery=" order by fname asc ";
+   $orderQuery=" order by stu.fname asc ";
 
 }
 $searchValue = $_POST['search']['value']; // Search value
@@ -28,21 +28,25 @@ if($searchValue != ''){
    or stu.lname like '%".$searchValue."%'
    ) ";
 }
+
 ## Total number of records without filtering
-$sel = mysqli_query($conn,"select count(*) as totalcount from student as stu ,student_preference_audit as stu_pref 
-inner join student_audit as sa on sa.email_id=stu_pref.email_id 
-where stu.email_id=stu_pref.email_id and stu_pref.allocate_status=0 and sa.currently_active=0 ");
+$sel = mysqli_query($conn,"select count(*) as totalcount from student as stu 
+inner join student_preference_audit as stu_pref on stu.email_id=stu_pref.email_id
+where stu_pref.allocate_status=0 and stu_pref.currently_active=1");
 $records = mysqli_fetch_assoc($sel);
 $totalRecords = $records['totalcount'];
 
 ## Total number of record with filtering
 // $sel = mysqli_query($conn,"select count(*) as totalcountfilters from audit_course a INNER JOIN department d ON a.dept_id=d.dept_id WHERE currently_active=0 ".$searchQuery);
-$sel = mysqli_query($conn,"select count(*) as totalcountfilters from student as stu ,student_preference_audit as stu_pref 
-inner join student_audit as sa on sa.email_id=stu_pref.email_id 
-where stu.email_id=stu_pref.email_id and stu_pref.allocate_status=0 and sa.currently_active=0 "
-.$searchQuery.$orderQuery." limit ".$row.",".$rowperpage);
+$sel = mysqli_query($conn,"select count(*) as totalcountfilters from student as stu 
+inner join student_preference_audit as stu_pref on stu.email_id=stu_pref.email_id
+where stu_pref.allocate_status=0 and stu_pref.currently_active=1 "
+.$searchQuery.$orderQuery);
 $records = mysqli_fetch_assoc($sel);
 $totalRecordwithFilter = $records['totalcountfilters'];
+// $unallocated_form_filled=$totalRecordwithFilter-$totalRecords;
+// $unallocated_form_notfilled=$totalstudents-$totalRecordwithFilter;
+// $total_unallocated=$unallocated_form_filled+$unallocated_form_notfilled;
 ## Fetch records
 // $sql="select cname,cid,sem,dept_name,max,min,year, 
 //        (SELECT GROUP_CONCAT(dept_name SEPARATOR ', ') FROM audit_course_applicable_dept aad 
@@ -53,9 +57,9 @@ $totalRecordwithFilter = $records['totalcountfilters'];
 
 
 
-$sql="select stu.fname,stu.lname,stu.email_id from student as stu ,student_preference_audit as stu_pref 
-inner join student_audit as sa on sa.email_id=stu_pref.email_id 
-where stu.email_id=stu_pref.email_id and stu_pref.allocate_status=0 and sa.currently_active=0 "
+$sql="select stu.fname,stu.lname,stu.email_id from student as stu 
+inner join student_preference_audit as stu_pref on stu.email_id=stu_pref.email_id
+where stu_pref.allocate_status=0 and stu_pref.currently_active=1"
 .$searchQuery.$orderQuery." limit ".$row.",".$rowperpage;
 
 //select stu.fname,stu.lname,stu.email_id from audit_course as c inner join e55dda_student_audit as stu_audit on c.cid=stu_audit.cid inner join student as stu on stu.email_id=stu_audit.email_id".$searchQuery. $orderQuery." limit ".$row.",".$rowperpage;
--- AC_EC (editable)/institutional_coordinator/student_allocation/audit_course/previous_allocation.php
+++ AC_EC (editable)/institutional_coordinator/student_allocation/audit_course/previous_allocation.php
@@ -24,29 +24,28 @@ if($searchValue != ''){
    //      sem like '%".$searchValue."%' or year like '%".$searchValue."%' or
    //      cname like'%".$searchValue."%' or dept_name like '%".$searchValue."%'
    //      ) ";
-   $searchQuery = " and (stu.email_id like '%".$searchValue."%' or stu.fname like '%".$searchValue."%' 
-   or stu.lname like '%".$searchValue."%'
+   $searchQuery = " and (c.cid like '%".$searchValue."%' or 
+   c.cname like '%".$searchValue."%' or stu.fname like '%".$searchValue."%' or
+   stu.lname like'%".$searchValue."%'
    ) ";
 }
 
 ## Total number of records without filtering
-$sel = mysqli_query($conn,"select count(*) as totalcount from student as stu 
-inner join student_preference_audit as stu_pref on stu.email_id=stu_pref.email_id
-where stu_pref.allocate_status=0 and stu_pref.currently_active=1");
+$sel = mysqli_query($conn,"select count(*) as totalcount from audit_course_log as c 
+inner join student_audit_log as stu_audit 
+on c.cid=stu_audit.cid inner join student as stu on stu.email_id=stu_audit.email_id ");
 $records = mysqli_fetch_assoc($sel);
 $totalRecords = $records['totalcount'];
 
 ## Total number of record with filtering
 // $sel = mysqli_query($conn,"select count(*) as totalcountfilters from audit_course a INNER JOIN department d ON a.dept_id=d.dept_id WHERE currently_active=0 ".$searchQuery);
-$sel = mysqli_query($conn,"select count(*) as totalcountfilters from student as stu 
-inner join student_preference_audit as stu_pref on stu.email_id=stu_pref.email_id
-where stu_pref.allocate_status=0 and stu_pref.currently_active=1 "
-.$searchQuery.$orderQuery." limit ".$row.",".$rowperpage);
+$sel = mysqli_query($conn,"select count(*) as totalcountfilters from audit_course_log as c 
+inner join student_audit_log as stu_audit 
+on c.cid=stu_audit.cid inner join student as stu on stu.email_id=stu_audit.email_id 
+".$searchQuery. $orderQuery);
 $records = mysqli_fetch_assoc($sel);
 $totalRecordwithFilter = $records['totalcountfilters'];
-// $unallocated_form_filled=$totalRecordwithFilter-$totalRecords;
-// $unallocated_form_notfilled=$totalstudents-$totalRecordwithFilter;
-// $total_unallocated=$unallocated_form_filled+$unallocated_form_notfilled;
+
 ## Fetch records
 // $sql="select cname,cid,sem,dept_name,max,min,year, 
 //        (SELECT GROUP_CONCAT(dept_name SEPARATOR ', ') FROM audit_course_applicable_dept aad 
@@ -55,14 +54,10 @@ $totalRecordwithFilter = $records['totalcountfilters'];
 //         from audit_course a INNER JOIN department d ON a.dept_id=d.dept_id WHERE currently_active=0 "
 //         .$searchQuery. $orderQuery." limit ".$row.",".$rowperpage;
 
-
-
-$sql="select stu.fname,stu.lname,stu.email_id from student as stu 
-inner join student_preference_audit as stu_pref on stu.email_id=stu_pref.email_id
-where stu_pref.allocate_status=0 and stu_pref.currently_active=1"
-.$searchQuery.$orderQuery." limit ".$row.",".$rowperpage;
-
-//select stu.fname,stu.lname,stu.email_id from audit_course as c inner join e55dda_student_audit as stu_audit on c.cid=stu_audit.cid inner join student as stu on stu.email_id=stu_audit.email_id".$searchQuery. $orderQuery." limit ".$row.",".$rowperpage;
+$sql="select c.cid,c.cname,stu.fname,stu.lname,stu.email_id,stu_audit.sem,stu_audit.year 
+from audit_course_log as c inner join student_audit_log as stu_audit 
+on c.cid=stu_audit.cid inner join student as stu on stu.email_id=stu_audit.email_id 
+".$searchQuery. $orderQuery." limit ".$row.",".$rowperpage;
 $courseRecords = mysqli_query($conn, $sql);
 $data = array();
 $count=0;
@@ -73,12 +68,13 @@ while ($row = mysqli_fetch_assoc($courseRecords)) {
                         <label class="custom-control-label" for="selectrow_upcoming'.$count.'"></label>
                      </div>',
 					 
-      
+      "cname"=>$row['cname'],
+	  "cid"=>$row['cid'],
+	  "sem"=>$row['sem'],
       "fname"=>$row['fname'],
 	  "lname"=>$row['lname'],
       "email_id"=>$row['email_id'],
-	  "status"=>"FILLED",
-      
+      "year"=>$row['year'],
       
       "action"=>'<button type="button" class="btn btn-primary icon-btn action-btn">
                     <i class="fas fa-tools"></i>
@@ -86,7 +82,6 @@ while ($row = mysqli_fetch_assoc($courseRecords)) {
    );
    $count++;
 }
-
 
 ## Response
 $response = array(
