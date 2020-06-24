187a012d59e8aa9297dd35184af2db418449888f
dc480506 <dev.c@somaiya.edu>
2020-06-24 18:47:11 +0530

    Marks page upload by either rollno or emailid possible and some minor fixes

--- AC_EC (editable)/institutional_coordinator/student_mark/bulkUpload/student_marks.py
+++ AC_EC (editable)/institutional_coordinator/student_mark/bulkUpload/student_marks.py
@@ -2,17 +2,18 @@ import pymysql,xlrd,sys,re
 mapper={
 		"sem":0,
 		"year":1,
-		"email":2,
-        "marks":3,
-        "file_location":4,
-        "host":5,
-        "username":6,
-        "password":7,
-        "dbname":8,
+		"insert_by":2,
+		"insert_col_name":3,
+        "marks":4,
+        "file_location":5,
+        "host":6,
+        "username":7,
+        "password":8,
+        "dbname":9,
         }
 header=[]
 header_id={}
-start_col=2
+start_col=3
 argument=list(map(str.strip, sys.argv[1].strip('[]').split(',')))
 n=len(argument)
 class Gpaoutofbounds(Exception):
@@ -31,13 +32,15 @@ try:
 except Exception as e:
     print(re.findall(r"'(.*?)'",str(e),)[0]+" is not a column in the uploaded sheet")
     sys.exit(0)
-
-insert="""INSERT into student_marks(email_id,sem,year,gpa) VALUES(%s,%s,%s,%s);"""
+if argument[mapper['insert_by']]=='rollno':
+	insert="""INSERT into student_marks(email_id,rollno,sem,year,gpa) SELECT email_id,%s,%s,%s,%s FROM student WHERE rollno=%s;"""
+elif argument[mapper['insert_by']]=='email_id':
+	insert="""INSERT into student_marks(email_id,rollno,sem,year,gpa) SELECT %s,rollno,%s,%s,%s FROM student WHERE email_id=%s;"""
 sem=argument[mapper['sem']]
 year=argument[mapper['year']]
 try:
 	for x in range (1,data.nrows):
-		email=data.cell(x,header_id[argument[mapper['email']].lower()]).value
+		insert_col_name=data.cell(x,header_id[argument[mapper['insert_col_name']].lower()]).value
 		#print(email)
 		marks=data.cell(x,header_id[argument[mapper['marks']].lower()]).value
 		try:
@@ -45,10 +48,10 @@ try:
 				raise Gpaoutofbounds
 				break
 		except Gpaoutofbounds:
-			print('Gpa of student '+email+' is '+str(marks)+' which is greater than 10')
+			print('Gpa of student '+insert_col_name+' is '+str(marks)+' which is greater than 10')
 			sys.exit(0)
 		#print(rollno)
-		values=(email,sem,year,marks)
+		values=(insert_col_name,sem,year,marks,insert_col_name)
 		#executing query
 		cursor.execute(insert,values)
 except Exception as e:
