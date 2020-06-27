8ca3b42009ae7f745103a75feecb8c997634f226
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-06-27 19:25:00 +0530

    course mapping dropdowns

---
+++ AC_EC (editable)/institutional_coordinator/audit_course/loadModal/audit_modal_mapping_utils.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once('../../verify.php');
+include_once('../../../config.php');
+
+$dataFor = $_POST["dataFor"];
+
+if ($dataFor == "semester") {
+    $year = $_POST['year'];
+    $response = "";
+    $sql = "(select distinct(sem) from audit_course where year = '" . $year . "')" . " union " . "(select distinct(sem) from audit_course_log where year = '" . $year . "')";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $response .= '<option value="' . $row["sem"] . '">' . $row["sem"] . '</option>';
+    }
+    echo $response;
+} else if ($dataFor == "courses") {
+    $year = $_POST['year'];
+    $sem = $_POST['sem'];
+    $response = "";
+    $sql = "(select cid,cname from audit_course where year = '" . $year . "' and sem = '" . $sem . "')" . " UNION " . "(select cid,cname from audit_course where year = '" . $year . "' and sem = '" . $sem . "')";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $response .= '<option value="' . $row["cid"] . '">' . $row["cname"] . '</option>';
+    }
+    echo $response;
+}
