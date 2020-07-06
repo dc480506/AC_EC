ac9626185ca3b90eeeb87328f5f6075c96848de6
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-07-06 19:05:58 +0530

    edit and delete course type backend

--- AC_EC (editable)/institutional_coordinator/ic_queries/addcourse_type_queries.php
+++ AC_EC (editable)/institutional_coordinator/ic_queries/addcourse_type_queries.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['email']) && $_SESSION['role'] == "inst_coor") {
 
         mysqli_query($conn, $sql) or die(mysqli_error($conn));
         echo "added";
-    } else if ($_POST['get_course_types']) {
+    } else if (isset($_POST['get_course_types'])) {
         $draw = $_POST['draw'];
         $row = $_POST['start'];
         $rowperpage = $_POST['length']; // Rows display per page
@@ -51,7 +51,7 @@ if (isset($_SESSION['email']) && $_SESSION['role'] == "inst_coor") {
                 "select-cbox" => '<div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input selectrow" id="selectrow' . $count . '">
                         <label class="custom-control-label" for="selectrow' . $count . '"></label>
-                     </div>',
+                        </div>',
                 "course_type_id" => $row['id'],
                 "name" => $row['name'],
                 "program" => $row['program'],
@@ -62,10 +62,10 @@ if (isset($_SESSION['email']) && $_SESSION['role'] == "inst_coor") {
                 // "action"=>'<button type="button" class="btn btn-primary icon-btn action-btn" data-toggle="modal" data-target="#exampleModalCenter'.$count.'">
                 //                <i class="fas fa-tools"></i>
                 //             </button>'
-                "action" => '<div class="row"><button type="button" id="edit-button" class="btn btn-primary mx-1 icon-btn action-btn">
-                     <i class="fas fa-pen   "></i>
-                  </button><button type="button" id="delete-button" class="btn btn-danger mx-1 icon-btn action-btn">
-                     <i class="fas fa-trash   "></i>
+                "action" => '<div class="row"><button type="button" id="" class="btn btn-primary mx-1 icon-btn action-btn edit-button">
+                <i class="fas fa-pen   "></i>
+                  </button><button type="button" id="" class="btn btn-danger mx-1 icon-btn action-btn delete-button">
+                  <i class="fas fa-trash   "></i>
                   </button></div>',
 
             );
@@ -80,5 +80,19 @@ if (isset($_SESSION['email']) && $_SESSION['role'] == "inst_coor") {
         );
 
         echo json_encode($response);
+    } elseif (isset($_POST["delete_course_type"])) {
+        $course_type_id = mysqli_escape_string($conn, $_POST['course_type_id']);
+        $sql = "DELETE from course_types where id = '$course_type_id'";
+        mysqli_query($conn, $sql) or die(mysqli_error($conn));
+        echo "deleted";
+    } else if (isset($_POST["edit_course_type"])) {
+        $course_type_id = mysqli_escape_string($conn, $_POST['courseTypeId']);
+        $course_type_name = mysqli_escape_string($conn, $_POST['courseTypeName']);
+        mysqli_escape_string($conn, $_POST['courseTypeId']);
+        $sql = "UPDATE course_types set name='$course_type_name' where id = '$course_type_id'";
+        mysqli_query($conn, $sql) or die(mysqli_error($conn));
+        echo "edited";
+    } else {
+        die(json_encode($_POST));
     }
 }
