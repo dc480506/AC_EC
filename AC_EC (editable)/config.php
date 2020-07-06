38c7cbf3247a4d191b0e4a34f890af6a1c6ba22e
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-07-06 16:05:24 +0530

    bulk upload functional in add student

--- AC_EC (editable)/config.php
+++ AC_EC (editable)/config.php
@@ -6,7 +6,7 @@ $dbname="ac_ec";
 $port="3306";
 $CLIENT_ID="444425785443-5mh44gn88jrf46t217t7i4m62r4ui1ro.apps.googleusercontent.com";
 $google_api_path="../../../google-api-php-client-2.4.0/";
-$base_dir="D:/xampp/uploads/AC_EC\\";
+$base_dir="C:/xampp/uploads/AC_EC\\";
 $conn = new mysqli($servername,$username,$password,$dbname,$port);
 $exclude_dept="6";
 $other_pref_cols_audit_count=8;
@@ -14,4 +14,4 @@ $hash_key="35621";
 if($conn->connect_error)
 {
 	die("Connection: ".$conn->connect_error);
-}?>
+}
--- AC_EC (editable)/institutional_coordinator/adduser/bulkUpload/addstudent_queries.php
+++ AC_EC (editable)/institutional_coordinator/adduser/bulkUpload/addstudent_queries.php
@@ -1,26 +1,26 @@
 <?php
 include_once("../../../config.php");
 include_once("../../verify.php");
-        $fname=mysqli_escape_string($conn,$_POST['fname']);
-        $mname=mysqli_escape_string($conn,$_POST['mname']);
-        $lname=mysqli_escape_string($conn,$_POST['lname']);
-        $sem=mysqli_escape_string($conn,$_POST['semester']);
-        $year=mysqli_escape_string($conn,$_POST['year']);
-        $dep=mysqli_escape_string($conn,$_POST['department']);
-        $email=mysqli_escape_string($conn,$_POST['email']);
-        $rno=mysqli_escape_string($conn,$_POST['rno']);
+$fname = mysqli_escape_string($conn, $_POST['fname']);
+$mname = mysqli_escape_string($conn, $_POST['mname']);
+$lname = mysqli_escape_string($conn, $_POST['lname']);
+$sem = mysqli_escape_string($conn, $_POST['semester']);
+$year = mysqli_escape_string($conn, $_POST['year']);
+$dep = mysqli_escape_string($conn, $_POST['department']);
+$email = mysqli_escape_string($conn, $_POST['email']);
+$rno = mysqli_escape_string($conn, $_POST['rno']);
+$program = mysqli_escape_string($conn, $_POST['program']);
 
-        $file_name=$_FILES['Uploadfile']['name'];
-        $target_location=$base_dir.$file_name;
-        move_uploaded_file( $_FILES['Uploadfile']['tmp_name'], $target_location);
-        date_default_timezone_set('Asia/Kolkata');
-        $timestamp=date("Y-m-d H:i:s");
-       $args='["'.$_SESSION['email'].'","'.$timestamp.'","'.$fname.'","'.$lname.'","'.$mname.'","'.$sem.'","'.$year.'","'.$dep.'","'.$email.'","'.$rno.'","'.$target_location.'","'.$servername.'","'.$username.'","'.$password.'","'.$dbname.'"]';
-        // echo $db;
-        $cmd='python student.py '.$args;
-        // $command = escapeshellcmd($cmd);
-        // echo $cmd;
-        $output = shell_exec($cmd." 2>&1");
-        echo $output;
-        exit();
-?>
+$file_name = $_FILES['Uploadfile']['name'];
+$target_location = $base_dir . $file_name;
+move_uploaded_file($_FILES['Uploadfile']['tmp_name'], $target_location);
+date_default_timezone_set('Asia/Kolkata');
+$timestamp = date("Y-m-d H:i:s");
+$args = '["' . $_SESSION['email'] . '","' . $timestamp . '","' . $fname . '","' . $lname . '","' . $mname . '","' . $sem . '","' . $year . '","' . $dep . '","' . $email . '","' . $rno . '","' . $target_location . '","' . $servername . '","' . $username . '","' . $password . '","' . $dbname . '","' . $program . '"]';
+// echo $db;
+$cmd = 'python student.py ' . $args;
+// $command = escapeshellcmd($cmd);
+// echo $cmd;
+$output = shell_exec($cmd . " 2>&1");
+echo $output;
+exit();
