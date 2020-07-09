48c6de286f3520456079dc11b69686354ffb2c79
Sukhada Virkar <sukhada9987@gmail.com>
2020-07-09 19:58:44 +0530

    created modal to view courses taught by each faculty on internal faculty page

--- AC_EC (editable)/institutional_coordinator/loadAdditionalInfo/additional_info_internal_faculty.php
+++ AC_EC (editable)/institutional_coordinator/loadAdditionalInfo/additional_info_internal_faculty.php
@@ -27,6 +27,20 @@
         //         $i++;
         //     }
         // }
+
+            //get courses taught
+            $email_id=$data['email_id'];
+            // echo $email_id;
+            $sql1="SELECT `cname` from course c where c.cid in (select `cid` from faculty_course_alloted where currently_active=1 and email_id='$email_id')";
+        
+            $result1 = mysqli_query($conn,$sql1);
+            $str=array();
+            
+            while($row1=mysqli_fetch_assoc($result1))
+            {
+                array_push($str, $row1['cname']);
+            }
+            
         echo '
             <div class="form-row">
                 <div class="form-group col-md-4">
@@ -37,18 +51,60 @@
                     <label for="added_on"><b>Added on: </b></label>
                     <small><span>'.$timestamp.'</span></small>
                 </div>
-                <div class="form-group col-md-4">
-                    <label for="profile"><b>Profile: </b></label>
-                    <a href="#">View</a>
-                </div>
+               
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="Courses_info"><b>Courses Info: </b></label>
-                    <a href="#">View</a>
+                    <a data-toggle="modal" href="#myModal">View</a>
                 </div>
             </div>
+
+         
         ';
+        ?>
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">Additonal Faculty details</h5>
+                        <button type="button" class="close" id="close_add_form_cross" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                    
+                    
+                    <?php if(count($str)>0)
+                    {?>
+                     <h5 class="text-primary">Courses alloted currently:</h5>
+                    <br>
+                    
+                    <ul>
+                    <?php foreach($str as $val) {
+                       ?><li><?php echo $val;?></li> 
+                    <?php } ?>
+           </ul>
+           <?php
+           }
+           else
+           {?>
+           <h6 class="text-danger">No course alloted currently.</h6>
+           <?php
+           }?>
+
+                    </div>
+                    <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal" name="close">Close</button>
+                                
+                            </div>
+
+                            </div>
+                            </div>
+                            </div>
+      <?php  
     }
  }
 ?>
+
+                            
