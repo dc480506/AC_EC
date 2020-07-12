2bb8029b1535613421e94bf26eaa47f31f9d58b4
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-07-12 16:02:58 +0530

    added sem constraint in current courses query

--- AC_EC (editable)/student/index.php
+++ AC_EC (editable)/student/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include('../config.php');
 include_once('verify.php');
 include('../includes/header.php');
@@ -12,46 +12,46 @@ include('../includes/header.php');
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12 grid-margin stretch-card mt-2">
-             <?php 
-                // inserting the my profile card
+            <?php
+            // inserting the my profile card
 
 
-                $email = $_SESSION['email'];
-                $fname = 'fname';
-                $lname = 'lname';
-                $mname= 'mname';
-                $department = 'department';
-                // $mobile = '9876543201';
-                $roll='rollno';
-                $yoa='2018';
-                $program="program";
-                $current_sem='3';
+            $email = $_SESSION['email'];
+            $fname = 'fname';
+            $lname = 'lname';
+            $mname = 'mname';
+            $department = 'department';
+            // $mobile = '9876543201';
+            $roll = 'rollno';
+            $yoa = '2018';
+            $program = "program";
+            $current_sem = '3';
 
-                $query="SELECT s.fname,s.mname,s.lname,s.email_id,d.dept_name,s.program,s.rollno,s.year_of_admission,s.current_sem FROM student AS s,department AS d WHERE s.email_id='$email' AND s.dept_id=d.dept_id";
-                if ($result = mysqli_query($conn, $query)) {
-                    $rowcount = mysqli_num_rows($result);
-                    if ($rowcount == 1) {
-                        $row = mysqli_fetch_assoc($result);
-                        $fname = $row['fname'];
-                        $lname = $row['lname'];
-                        $mname= $row['mname'];
-                        $roll=$row['rollno'];
-                        $department=$row['dept_name'];
-                        $yoa=$row['year_of_admission'];
-                        $program=$row['program'];
-                        $current_sem=$row['current_sem'];
-                    }
+            $query = "SELECT s.fname,s.mname,s.lname,s.email_id,d.dept_name,s.program,s.rollno,s.year_of_admission,s.current_sem FROM student AS s,department AS d WHERE s.email_id='$email' AND s.dept_id=d.dept_id";
+            if ($result = mysqli_query($conn, $query)) {
+                $rowcount = mysqli_num_rows($result);
+                if ($rowcount == 1) {
+                    $row = mysqli_fetch_assoc($result);
+                    $fname = $row['fname'];
+                    $lname = $row['lname'];
+                    $mname = $row['mname'];
+                    $roll = $row['rollno'];
+                    $department = $row['dept_name'];
+                    $yoa = $row['year_of_admission'];
+                    $program = $row['program'];
+                    $current_sem = $row['current_sem'];
                 }
-                
-                // $result= mysqli_query($conn,$sql);
-                // $row= mysqli_fetch_array($result,MYSQLI_ASSOC);
-                // $count= mysqli_num_rows($result);
-                // echo 'hello';
-                // $_SESSION['sem']=$row['current_sem'];
-                // $_SESSION['rollno']=$row['rollno'];
-                // $_SESSION['year']=$row['year'];
-                // if($count==1)
-                // {
+            }
+
+            // $result= mysqli_query($conn,$sql);
+            // $row= mysqli_fetch_array($result,MYSQLI_ASSOC);
+            // $count= mysqli_num_rows($result);
+            // echo 'hello';
+            // $_SESSION['sem']=$row['current_sem'];
+            // $_SESSION['rollno']=$row['rollno'];
+            // $_SESSION['year']=$row['year'];
+            // if($count==1)
+            // {
             ?>
             <div class="card">
                 <div class="card-body">
@@ -63,17 +63,17 @@ include('../includes/header.php');
                         <div class="col-md-6">
                             <div class="row">
                                 <div class="col-md-6">
-                                    <p class="text-dark"> <span><b>Name : </b></span><?php echo $fname . ' '. $mname .' ' . $lname; ?></p>
-                                    <p class="text-dark"> <span><b>Email : </b></span><?php echo $email;?></p>
-                                    <p class="text-dark"> <span><b>Year of Admission: </b></span><?php echo $yoa;?></p>
+                                    <p class="text-dark"> <span><b>Name : </b></span><?php echo $fname . ' ' . $mname . ' ' . $lname; ?></p>
+                                    <p class="text-dark"> <span><b>Email : </b></span><?php echo $email; ?></p>
+                                    <p class="text-dark"> <span><b>Year of Admission: </b></span><?php echo $yoa; ?></p>
                                     <!-- <p class="text-dark"> <span><b>Mobile No. : </b></span><?php echo $mobile; ?> -->
                                 </div>
                                 <div class="col-md-6">
-                                <p class="text-dark"> <span><b>Program : </b></span><?php echo $program;?></p>
-                                    <p class="text-dark"> <span><b>Department : </b></span><?php echo $department;?></p>
-                                    <p class="text-dark"> <span><b>Roll No. : </b></span><?php echo $roll;?></p>
-                                    <p class="text-dark"> <span><b>Current Semester : </b></span><?php echo $current_sem;?></p>
-                                 </p>
+                                    <p class="text-dark"> <span><b>Program : </b></span><?php echo $program; ?></p>
+                                    <p class="text-dark"> <span><b>Department : </b></span><?php echo $department; ?></p>
+                                    <p class="text-dark"> <span><b>Roll No. : </b></span><?php echo $roll; ?></p>
+                                    <p class="text-dark"> <span><b>Current Semester : </b></span><?php echo $current_sem; ?></p>
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -119,42 +119,40 @@ include('../includes/header.php');
                                 </tr>
                             </tfoot>
                             <tbody>
-                            <?php 
+                                <?php
 
-                                $sql="SELECT course_types.name,course.cid,course.cname,sa.sem,sa.year,(SELECT count(*) FROM 
-                                student_course_alloted AS sa WHERE currently_active=1 AND sa.cid=course.cid) as no_of_allocated,
+                                $sql = "SELECT course_types.name,course.cid,course.cname,sa.sem,sa.year,
                                 sa.complete_status,course.year FROM course_types,course AS course INNER Join student_course_alloted
                                 AS sa on sa.cid=course.cid AND sa.sem=course.sem AND course.year=sa.year WHERE sa.email_id='$email' 
-                                AND sa.currently_active='1' AND course_types.id=course.course_type_id";
+                                AND sa.currently_active='1' and sa.sem='$current_sem' AND course_types.id=course.course_type_id";
 
 
-                                $result =mysqli_query($conn,$sql);
-                                $count=0;
+                                $result = mysqli_query($conn, $sql);
+                                $count = 0;
 
-                                while($row = mysqli_fetch_array($result)) 
-                                 {  ?>
+                                while ($row = mysqli_fetch_array($result)) {  ?>
                                     <tr>
-                                        <td><?php echo $row['name'];?></td>
+                                        <td><?php echo $row['name']; ?></td>
 
-                                        <td><?php echo $row['cname'];?></td>
-                                    
-                                        <td><?php echo $row['cid'];?></td>
+                                        <td><?php echo $row['cname']; ?></td>
 
-                                        <td><?php echo $row['year'];?></td>
-                                    
-                                        <td><?php echo $row['sem'];?></td>
-                                    
+                                        <td><?php echo $row['cid']; ?></td>
+
+                                        <td><?php echo $row['year']; ?></td>
+
+                                        <td><?php echo $row['sem']; ?></td>
+
                                         <td>
-                                            <?php if($row['complete_status']==1)
-                                                    echo "Complete";
-                                                   else
-                                                    echo "Incomplete";
+                                            <?php if ($row['complete_status'] == 1)
+                                                echo "Complete";
+                                            else
+                                                echo "Incomplete";
                                             ?>
-                                        </td> 
+                                        </td>
                                     </tr>
 
-                                 <?php } ?>
-                                 
+                                <?php } ?>
+
                             </tbody>
                         </table>
                     </div>
