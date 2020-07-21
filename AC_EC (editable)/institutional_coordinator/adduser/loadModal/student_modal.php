f7d2b43d722518416cd7432cdfbe69e6924d1cb5
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-07-21 19:18:22 +0530

    roles on student modal operation update and delete

--- AC_EC (editable)/institutional_coordinator/adduser/loadModal/student_modal.php
+++ AC_EC (editable)/institutional_coordinator/adduser/loadModal/student_modal.php
@@ -1,27 +1,28 @@
 <?php
-    // echo 'Hi';
+// echo 'Hi';
 session_start();
-if(isset($_SESSION['email']) && $_SESSION['role']=='inst_coor'){
-    // echo 'Hi';
+$allowed_roles = array("inst_coor", "faculty_co", "HOD");
+if (isset($_SESSION['email']) && in_array($_SESSION['role'], $allowed_roles)) {
     include_once('../../../config.php');
-    $data = json_decode(file_get_contents("php://input"),true); 
-    $email_id=mysqli_escape_string($conn,$data['email_id']);
-    $name=mysqli_escape_string($conn,$data['name']);
-    $dept_name=mysqli_escape_string($conn,$data['dept_name']);
-    $result = mysqli_query($conn,"select rollno,fname,mname,lname,dept_id,year_of_admission,current_sem from student WHERE email_id='$email_id'");
-    $row=mysqli_fetch_assoc($result);
-    $rollno=$row['rollno'];
-    $fname=$row['fname'];
-    $mname=$row['mname'];
-    $lname=$row['lname'];
-    $dept_id=$row['dept_id'];
-    $year_of_admission=$row['year_of_admission'];
-    $current_sem=$row['current_sem'];
-    
-    $result = mysqli_query($conn,"select academic_year from current_sem_info WHERE currently_active=1");
-    $row=mysqli_fetch_assoc($result);
-    $year=$row['academic_year'];
-      echo '<div class="modal fade mymodal" id="update-del-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle1" aria-hidden="true">
+    // echo 'Hi';
+    $data = json_decode(file_get_contents("php://input"), true);
+    $email_id = mysqli_escape_string($conn, $data['email_id']);
+    $name = mysqli_escape_string($conn, $data['name']);
+    $dept_name = mysqli_escape_string($conn, $data['dept_name']);
+    $result = mysqli_query($conn, "select rollno,fname,mname,lname,dept_id,year_of_admission,current_sem from student WHERE email_id='$email_id'");
+    $row = mysqli_fetch_assoc($result);
+    $rollno = $row['rollno'];
+    $fname = $row['fname'];
+    $mname = $row['mname'];
+    $lname = $row['lname'];
+    $dept_id = $row['dept_id'];
+    $year_of_admission = $row['year_of_admission'];
+    $current_sem = $row['current_sem'];
+
+    $result = mysqli_query($conn, "select academic_year from current_sem_info WHERE currently_active=1");
+    $row = mysqli_fetch_assoc($result);
+    $year = $row['academic_year'];
+    echo '<div class="modal fade mymodal" id="update-del-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle1" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -44,9 +45,9 @@ if(isset($_SESSION['email']) && $_SESSION['role']=='inst_coor'){
                                 <form id="delete_student">
                                     <div class="form-group">
                                         <label for="exampleFormControlSelect1"><i class="text-danger">*This will delete all the information related to the students</i>
-                                            <br>Are you sure you want to delete the student with <br> name <i><small><b>'.$name.'</b></small></i>
-                                            ,Email ID <i><small><b>'.$email_id.'</small></b></i> ,rollno <i><small><b>'.$rollno.'</b></small></i>
-                                            And department <i><small><b>'.$dept_name.'</b></small></i>?
+                                            <br>Are you sure you want to delete the student with <br> name <i><small><b>' . $name . '</b></small></i>
+                                            ,Email ID <i><small><b>' . $email_id . '</small></b></i> ,rollno <i><small><b>' . $rollno . '</b></small></i>
+                                            And department <i><small><b>' . $dept_name . '</b></small></i>?
                                         </label>
                                         <br>
                                         <input type="hidden" name="email_id" value="' . $email_id . '">
@@ -74,7 +75,7 @@ if(isset($_SESSION['email']) && $_SESSION['role']=='inst_coor'){
                                         <div class="form-group col-md-6">
                                             <label for="email_id"><b>Email ID</b></label>
                                             <input type="email" class="form-control" required="required" placeholder="New Email Id" name="email_id_new" id="email_id_new" value="' . $email_id . '">
-                                            <input type="hidden" class="form-control"  name="email_id_old" id="email_id_old" value="' . $email_id. '">
+                                            <input type="hidden" class="form-control"  name="email_id_old" id="email_id_old" value="' . $email_id . '">
                                             <span id="error_email_id" class="text-danger"></span>
                                         </div>                                   
                                     </div>
@@ -82,40 +83,40 @@ if(isset($_SESSION['email']) && $_SESSION['role']=='inst_coor'){
                                         <div class="form-group col-md-6">
                                             <label for="rollno"><b>Roll No</b></label>
                                             <input type="text" class="form-control" required="required" placeholder="New Roll no" name="rollno_new" value="' . $rollno . '">
-                                            <input type="hidden" class="form-control"  name="rollno_old" value="' . $rollno. '">
+                                            <input type="hidden" class="form-control"  name="rollno_old" value="' . $rollno . '">
                                             <span id="error_rollno" class="text-danger"></span>
                                         </div>
                                         <div class="form-group col-md-6">
                                             <label for="year_of_admission"><b>Year</b></label>
                                             <select class="form-control" name="year_of_admission_new" id="year_of_admission_new">';
-                                            $sql = "SELECT academic_year FROM current_sem_info WHERE currently_active=1";
-                                            $result = mysqli_query($conn, $sql);
-                                            $row = mysqli_fetch_assoc($result);
-                                            $year1 = $row['academic_year'];
-                                            $year2 = $row['academic_year'];
-                                            for ($i = 0; $i < 2; $i++) {
-                                              $temp = explode('-', $year1)[0];
-                                              $temp += 1;
-                                              $temp2 = "" . ($temp + 1);
-                                              $year1 = $temp . "-" . substr($temp2, 2);
-                                              $year1_value=$temp;
-                                              echo '<option>' . $year1_value . '</option>';
-                                            }
-                                            for ($i = 0; $i < 4; $i++) {
-                                              if ($year == $year1) {
-                                                $year_dropdown .= "<option selected>" . $year1_value . "</option>";
-                                              }
-                                              $temp = explode('-', $year2)[0];
-                                              $year2_value = $temp;
-                                              $temp -= 1;
-                                              $temp2 = "" . ($temp + 1);
-                                              $year2 = $temp . "-" . substr($temp2, 2);
-                                            //   $year2_value = $temp;
-                                              echo '<option>' . $year2_value . '</option>';
-                                            }
-                                          
-                                            echo '</select>
-                                            <input type="hidden" class="form-control"  name="year_of_admission_old" value="' . $year_of_admission. '">
+    $sql = "SELECT academic_year FROM current_sem_info WHERE currently_active=1";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $year1 = $row['academic_year'];
+    $year2 = $row['academic_year'];
+    for ($i = 0; $i < 2; $i++) {
+        $temp = explode('-', $year1)[0];
+        $temp += 1;
+        $temp2 = "" . ($temp + 1);
+        $year1 = $temp . "-" . substr($temp2, 2);
+        $year1_value = $temp;
+        echo '<option>' . $year1_value . '</option>';
+    }
+    for ($i = 0; $i < 4; $i++) {
+        if ($year == $year1) {
+            $year_dropdown .= "<option selected>" . $year1_value . "</option>";
+        }
+        $temp = explode('-', $year2)[0];
+        $year2_value = $temp;
+        $temp -= 1;
+        $temp2 = "" . ($temp + 1);
+        $year2 = $temp . "-" . substr($temp2, 2);
+        //   $year2_value = $temp;
+        echo '<option>' . $year2_value . '</option>';
+    }
+
+    echo '</select>
+                                            <input type="hidden" class="form-control"  name="year_of_admission_old" value="' . $year_of_admission . '">
                                         </div>  
                                     </div>
                                     <div class="form-row">
@@ -124,38 +125,40 @@ if(isset($_SESSION['email']) && $_SESSION['role']=='inst_coor'){
                                             <div class="form-group">
                                                 <select class="form-control" required name="dept_id">
                                                     ';
-                                                    $sql = "SELECT * FROM department";
-                                                    $result = mysqli_query($conn, $sql);
-                                                    while ($row = mysqli_fetch_assoc($result)) {
-                                                        if($row['dept_id']== $dept_id)
-                                                        {
-                                                            echo '<option selected value="'.$row['dept_id'].'">' . $row['dept_name'] . '</option>';
-                                                        }
-                                                       else{
-                                                        echo '<option  value="'.$row['dept_id'].'">' . $row['dept_name'] . '</option>';
-                                                       }
-                                                    
-                                                    }
-                                                  
-                                                    echo '
+    if ($_SESSION['role'] == "inst_coor") {
+
+        $sql = "SELECT * FROM department";
+        $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($row['dept_id'] == $dept_id) {
+                echo '<option selected value="' . $row['dept_id'] . '">' . $row['dept_name'] . '</option>';
+            } else {
+                echo '<option  value="' . $row['dept_id'] . '">' . $row['dept_name'] . '</option>';
+            }
+        }
+    } else if (in_array($_SESSION['role'], array('faculty_co', "HOD"))) {
+        echo '<option  value="' . $_SESSION['dept_id'] . '">' . $_SESSION['dept_name'] . '</option>';
+    }
+
+    echo '
                                                 </select>
                                              </div> 
                                         </div>
                                         <div class="form-group col-md-6">
                                             <label for="current_sem"><b>Semester</b></label>
                                             <select class="form-control" required="required" name="current_sem_new" id="current_sem_new">';
-                                                $i = 1;
-                                                for ($i = 1; $i <= 8; $i++) {
-                                                    echo '<option value="';
-                                                    echo $i;
-                                                    echo '"';
-                                                    if ($i == $current_sem )
-                                                    echo ' selected';
-                                                    echo ">";
-                                                    echo $i;
-                                                    echo '</option>';
-                                                }
-                                                echo '</select>                                            
+    $i = 1;
+    for ($i = 1; $i <= 8; $i++) {
+        echo '<option value="';
+        echo $i;
+        echo '"';
+        if ($i == $current_sem)
+            echo ' selected';
+        echo ">";
+        echo $i;
+        echo '</option>';
+    }
+    echo '</select>                                            
                                         </div>                                    
                                     </div>
                                     
@@ -173,23 +176,21 @@ if(isset($_SESSION['email']) && $_SESSION['role']=='inst_coor'){
                     </div>
                 </div>
             </div>';
-            // $name=$fname.' '.$mname.' '.$lname;
+    // $name=$fname.' '.$mname.' '.$lname;
 
-                                // echo 'Hi';
+    // echo 'Hi';
 }
 ?>
 <script>
-    function SemestersOnly(input)
-    {
+    function SemestersOnly(input) {
         var regex = /[^1-8]/;
-        input.value = input.value.replace(regex,"");
+        input.value = input.value.replace(regex, "");
     }
-    function NumbersOnly(input)
-    {
-        var regex =/[a-z]/;
-        input.value = input.value.replace(regex,"");
-        var regex1 =/[2-9]/;
-        input.value = input.value.replace(regex1,"");
+
+    function NumbersOnly(input) {
+        var regex = /[a-z]/;
+        input.value = input.value.replace(regex, "");
+        var regex1 = /[2-9]/;
+        input.value = input.value.replace(regex1, "");
     }
-    
 </script>
