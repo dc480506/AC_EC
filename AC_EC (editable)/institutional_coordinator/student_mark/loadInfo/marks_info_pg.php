dac5b96feafb3409e149e5a155c2a786b81f186e
hasti-15 <hasti.ds@somaiya.edu>
2020-07-25 08:58:38 -0700

    Restriction on marks page

--- AC_EC (editable)/institutional_coordinator/student_mark/loadInfo/marks_info_pg.php
+++ AC_EC (editable)/institutional_coordinator/student_mark/loadInfo/marks_info_pg.php
@@ -4,15 +4,15 @@ include_once('../../../config.php');
 $draw = $_POST['draw'];
 $row = $_POST['start'];
 $rowperpage = $_POST['length']; // Rows display per page
-if(isset($_POST['order'])){
+if (isset($_POST['order'])) {
    $columnIndex = $_POST['order'][0]['column']; // Column index
    $columnName = $_POST['columns'][$columnIndex]['data']; // Column name
    $columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
-   $orderQuery=" order by $columnName $columnSortOrder ";
-}else{
+   $orderQuery = " order by $columnName $columnSortOrder ";
+} else {
    // $columnName='sem';
    // $columnSortOrder='asc';
-   $orderQuery=' order by year desc,sem,rollno asc ';
+   $orderQuery = ' order by year desc,sem,rollno asc ';
 }
 
 #filters
@@ -37,55 +37,63 @@ $searchValue = $_POST['search']['value']; // Search value
 
 ## Search 
 $searchQuery = "1";
-if($searchValue != ''){
-   $searchQuery = "( gpa like '%".$searchValue. "%' or dept_name like '%" . $searchValue . "%' or sem like '%".$searchValue."%' or m.rollno like '%".$searchValue."%' or m.email_id like '%".$searchValue."%' or year like '%".$searchValue."%')";
+if ($searchValue != '') {
+   $searchQuery = "( gpa like '%" . $searchValue . "%' or dept_name like '%" . $searchValue . "%' or sem like '%" . $searchValue . "%' or m.rollno like '%" . $searchValue . "%' or m.email_id like '%" . $searchValue . "%' or year like '%" . $searchValue . "%')";
 }
 
+$role_restriction = "";
+if ($_SESSION['role'] == 'faculty_co' || $_SESSION['role'] == 'HOD') {
+   $role_restriction = " and s.dept_id='{$_SESSION['dept_id']}' ";
+}
+
+
 ## Total number of records without filtering
-$sel = mysqli_query($conn,"select count(*) as totalcount from student_marks where program='PG' ");
+$query="select count(*) as totalcount from student_marks s where program='PG' ";
+$sel = mysqli_query($conn,$query);
+// echo $query;
 $records = mysqli_fetch_assoc($sel);
 $totalRecords = $records['totalcount'];
 
-$query="select count(*) as totalcountfilters from student_marks m INNER JOIN student s ON m.email_id=s.email_id INNER JOIN department d ON s.dept_id=d.dept_id WHERE m.program='PG' and ".$searchQuery . "&& (" . $filterQuery . ")";
-$sel = mysqli_query($conn,$query);
+$query = "select count(*) as totalcountfilters from student_marks m INNER JOIN student s ON m.email_id=s.email_id INNER JOIN department d ON s.dept_id=d.dept_id WHERE m.program='PG' " . $role_restriction. " and " . $searchQuery . "&& (" . $filterQuery . ")";
+$sel = mysqli_query($conn, $query);
 // echo $query;
 $records = mysqli_fetch_assoc($sel);
 $totalRecordwithFilter = $records['totalcountfilters'];
 
 ##Fetch Record
-$sql="select m.email_id,m.rollno,CONCAT(fname,' ',mname,' ',lname) as fullname,dept_name,sem,gpa,year
-from student_marks m INNER JOIN student s ON m.email_id=s.email_id INNER JOIN department d ON s.dept_id=d.dept_id WHERE m.program='PG' and "
-       .$searchQuery . " && (" . $filterQuery . ")". $orderQuery ." limit ".$row.",".$rowperpage;
+$sql = "select m.email_id,m.rollno,CONCAT(fname,' ',mname,' ',lname) as fullname,dept_name,sem,gpa,year
+from student_marks m INNER JOIN student s ON m.email_id=s.email_id INNER JOIN department d ON s.dept_id=d.dept_id WHERE m.program='PG' " . $role_restriction. " && "
+   . $searchQuery . " && (" . $filterQuery . ")" . $orderQuery . " limit " . $row . "," . $rowperpage;
 $studentRecords = mysqli_query($conn, $sql);
 $data = array();
-$count=0;
+$count = 0;
 while ($row = mysqli_fetch_assoc($studentRecords)) {
-    
-   $data[] = array( 
+
+   $data[] = array(
       // "select-cbox"=>'<input type="checkbox">',
-      "select-cbox"=>'<div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input selectrow" id="selectrow'.$count.'">
-                        <label class="custom-control-label" for="selectrow'.$count.'"></label>
+      "select-cbox" => '<div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input selectrow" id="selectrow' . $count . '">
+                        <label class="custom-control-label" for="selectrow' . $count . '"></label>
                      </div>',
-      "email_id"=>$row['email_id'],
-      "rollno"=>$row['rollno'],
-      "fullname"=>$row['fullname'],
-      "dept_name"=> $row['dept_name'],
-      "sem"=>$row['sem'],
-      "year"=>$row['year'],
-      "gpa"=>$row['gpa'],
-      "action"=>'<button type="button" class="btn btn-primary icon-btn action-btn">
+      "email_id" => $row['email_id'],
+      "rollno" => $row['rollno'],
+      "fullname" => $row['fullname'],
+      "dept_name" => $row['dept_name'],
+      "sem" => $row['sem'],
+      "year" => $row['year'],
+      "gpa" => $row['gpa'],
+      "action" => '<button type="button" class="btn btn-primary icon-btn action-btn">
                     <i class="fas fa-tools"></i>
                 </button>',
-       );
+   );
    $count++;
 }
 ## Response
 $response = array(
-  "draw" => intval($draw),
-  "iTotalRecords" => $totalRecords,
-  "iTotalDisplayRecords" => $totalRecordwithFilter,
-  "aaData" => $data
+   "draw" => intval($draw),
+   "iTotalRecords" => $totalRecords,
+   "iTotalDisplayRecords" => $totalRecordwithFilter,
+   "aaData" => $data
 );
 
 echo json_encode($response);
