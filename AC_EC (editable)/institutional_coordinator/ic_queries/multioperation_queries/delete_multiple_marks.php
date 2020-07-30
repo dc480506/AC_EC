c0184afe10eff0fbab5cab1a2d502d2d5e478ff9
hiral72 <46996903+hiral72@users.noreply.github.com>
2020-07-30 21:11:38 +0530

    changes done

--- AC_EC (editable)/institutional_coordinator/ic_queries/multioperation_queries/delete_multiple_marks.php
+++ AC_EC (editable)/institutional_coordinator/ic_queries/multioperation_queries/delete_multiple_marks.php
@@ -9,25 +9,16 @@ if (isset($_SESSION['email']) && in_array($_SESSION['role'], $allowed_roles)) {
         $row=mysqli_fetch_assoc($result);
         $delete_data=$data['delete_data'];
         $del_condition=" (";
-        $del_condition2=" (";
-        // echo var_dump($delete_data);
         foreach($delete_data as $key=>$val){
-            $del_condition2.="email_id='".$val['email_id']."') OR (";
-            // echo var_dump($val);
             $del_condition.="email_id='".$val['email_id']."' AND ";
             $del_condition.="sem='".$val['sem']."' AND ";
             $del_condition.="year='".$val['year']."') OR (";
         }
-
-        $del_condition2=substr($del_condition2,0,strlen($del_condition2)-4);
-        $sql1="DELETE FROM login_role where ".$del_condition2;
-        // echo $del_condition;
         $del_condition=substr($del_condition,0,strlen($del_condition)-4);
-        // echo $del_condition;
         $sql="DELETE FROM student_marks WHERE ".$del_condition;
         mysqli_query($conn,$sql);
-        mysqli_query($conn,$sql1);
-        // echo $sql;
+
+
     }
 }
 ?>
