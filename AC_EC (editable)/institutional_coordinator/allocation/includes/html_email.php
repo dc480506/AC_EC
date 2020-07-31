a4d7a33f11de42365cbc201bdcd2bbff3ad3ac3c
Sukhada Virkar <sukhada9987@gmail.com>
2020-07-31 13:53:11 +0530

    changed design of allocation email in html_email.php

--- AC_EC (editable)/institutional_coordinator/allocation/includes/html_email.php
+++ AC_EC (editable)/institutional_coordinator/allocation/includes/html_email.php
@@ -3,48 +3,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href='https://fonts.googleapis.com/css?family=Acme' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Aref Ruqaa' rel='stylesheet'>
+    <!-- <link href='https://fonts.googleapis.com/css?family=Acme' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Aref Ruqaa' rel='stylesheet'> -->
     
-<script src="https://kit.fontawesome.com/57397afa58.js" crossorigin="anonymous"></script>
+<!-- <script src="https://kit.fontawesome.com/57397afa58.js" crossorigin="anonymous"></script> -->
 </head>
 <body>
-    <div class="card" style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);padding: 16px;
-    background-color: #fce8ec;">
-    <div align="center" style="font-size:22px;background-color:white;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);">
+    <div class="card" style="padding:50px; background-color:white;">
+    <div style=" background-color: #f5f5f5;">
+    <div style="font-size:22px;background-color:white;border:1px solid #e2e1e3;">
+   
+   <div style="border-bottom:1px solid #e2e1e3;padding:10px;">
    <br>
-    <img src="../../../vendor/img/somaiya_logo1.png" height="100px" width="100px" style="float:left;max-width:100%">
-    <img src="../../../vendor/img/somaiya_logo2.jpg" height="100px" width="100px" style="float:right;max-width:100%">
-<br>
-         <h2 style="color:black; font-family: 'Acme';">Allotment for Sem V OET Course</h2>
-         <br>
-    </div>
-    <br>
-    <div style="font-family: 'Aref Ruqaa';background-color:white;padding:10px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);">
-    <div style="background:#c9043c; padding:2px"></div>
-        <h3>Dear student,<br>
-        You have been successfully alloted an OET course as per your choices given.
-</h3>
+   
+   <img src="../../../vendor/img/somaiya_logo2.jpg" height="80px" width="100px" style="float:right;max-width:100%">
+   
+    <img src="../../../vendor/img/somaiya_logo1.png" height="80px" width="100px" style="float:left;max-width:100%">
         
-        <h2 style="color: black;">Allotment details&nbsp;<i class="fas fa-clipboard-list"></i></h2>
-        <h2 style="color:#c9043c;">You have been alloted Course_id - Flutter,<br> for Semester 3 of Academic Year 2020.<br></h2>
-        <h3>The alloted course was your 1st preference as per the preference form filled by you.</h3>
+    <h2 style="color:black; font-family: 'sans-serif';" align="center">
+    Allotment for Sem V OET Course</h2>
+        
+        </div>
+        
+        <div style="padding:60px;background-color:#f1f0f2">
+        <div style="background:#3d007a; padding:2px"></div>
+    <div style="font-family: 'Times New Roman';background-color:white;padding:20px;">
     
-    
-        <h3>The details about the commencement of course and faculty responsible will be conveyed to you soon through mail.</h3>
+        <h4>Dear student,<br>
+        You have been successfully alloted an OET course as per your choices given.
+</h4>
+        
+        <h4 style="color: black;">Allotment details:</h4>
+        <h4 style="color:#3d007a;">You have been alloted Course_id - Flutter,<br> for Semester 3 of Academic Year 2020.<br></h4>
+        <h4>The alloted course was your 1st preference as per the preference form filled by you.</h4>
+        <h4>The details about the commencement of course and faculty responsible will be conveyed to you soon through mail.</h4>
     </div>
     <br>
     
-    <div style="font-family: 'Aref Ruqaa';background-color:white;padding:10px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);">
-    
-    
-    
-    <h3>For any queries/doubts regarding the allotment, kindly contact HOD of your respective department.
+    <!-- <div style="font-family: 'Aref Ruqaa';background-color:white;padding:10px;">
+     -->
+    </div>
+    <div style="padding:40px;background-color:#f1f0f2">
+    <div style="background:#d5d4d6; padding:1px"></div>
+    <div style="background-color:#f1f0f2;padding:10px;">
+    <h4>For any queries/doubts regarding the allotment, kindly contact HOD of your respective department.
         <br><br>Regards,<br>SDC KJSCE.
-    </h3>
+    </h4>
+    </div>
+    </div>
     <!-- <img src="../../../vendor/img/som.png" height="150px" width="150px" style="max-width:100%;"> -->
+<!-- </div> -->
 </div>
-</div>
+</div>        
+    </div>
+    
+
 
     <!-- <div align="center" style="border: solid black 2px  max-width:100%;">
         
