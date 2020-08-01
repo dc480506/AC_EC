a8c2974dd3e4405056d3a85d1bbe23923e0a7fdd
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-08-01 12:12:42 +0530

    merge

--- AC_EC (editable)/institutional_coordinator/ic_queries/addfaculty_queries.php
+++ AC_EC (editable)/institutional_coordinator/ic_queries/addfaculty_queries.php
@@ -8,30 +8,6 @@ if (isset($_SESSION['email']) && in_array($_SESSION['role'], $allowed_roles)) {
         $email = mysqli_escape_string($conn, $_POST['email']);
         $sql = "DELETE FROM faculty WHERE email_id='$email'";
         echo $sql;
-<<<<<<< HEAD
-        mysqli_query($conn,$sql);
-    }else if(isset($_POST['update_faculty'])){
-        $name=mysqli_escape_string($conn,$_POST['name']);
-        $newemail=mysqli_escape_string($conn,$_POST['newemail']);
-        $oldemail=mysqli_escape_string($conn,$_POST['oldemail']);
-        $dept_id=mysqli_escape_string($conn,$_POST['dept']);
-        $eid=mysqli_escape_string($conn,$_POST['eid']);
-        $faculty_code=mysqli_escape_string($conn,$_POST['faculty_code']);
-        $accomplishment=mysqli_escape_string($conn,$_POST['accomplishment']);
-        $post=mysqli_escape_string($conn,$_POST['post']);
-        $names=explode(" ",$name);
-
-        $sql="UPDATE faculty SET fname='$names[0]',mname='$names[1]',lname='$names[2]',email_id='$newemail',faculty_code='$faculty_code',employee_id='$eid',dept_id='$dept_id',post='$post' WHERE email_id='$oldemail'";
-        mysqli_query($conn,$sql);
-
-    }else if(isset($_POST['add_faculty'])){
-        $name=mysqli_escape_string($conn,$_POST['name']);
-        $email=mysqli_escape_string($conn,$_POST['email']);
-        $dept_id=mysqli_escape_string($conn,$_POST['dept']);
-        $post=mysqli_escape_string($conn,$_POST['post']);
-        $faculty_code=mysqli_escape_string($conn,$_POST['faculty_code']);
-        $eid=mysqli_escape_string($conn,$_POST['eid']);
-=======
         mysqli_query($conn, $sql);
         mysqli_query($conn, "DELETE FROM login_role WHERE email_id='$email'");
     } else if (isset($_POST['update_faculty'])) {
@@ -59,61 +35,10 @@ if (isset($_SESSION['email']) && in_array($_SESSION['role'], $allowed_roles)) {
         $post = mysqli_escape_string($conn, $_POST['post']);
         $faculty_code = mysqli_escape_string($conn, $_POST['faculty_code']);
         $eid = mysqli_escape_string($conn, $_POST['eid']);
->>>>>>> dc92e723bb853cecd634325384b23c64a55b8156
         // $email=$_SESSION['email'];
         date_default_timezone_set('Asia/Kolkata');
         $timestamp = date("Y-m-d H:i:s");
 
-<<<<<<< HEAD
-        $sql="INSERT INTO faculty(`email_id`,`faculty_code`,`employee_id`, `fname`, `mname`, `lname`, `dept_id`, `post`, `username`,`added_by`,`timestamp`) VALUES ('$email','$faculty_code',$eid,'$names[0]','$names[1]','$names[2]','$dept_id', '$post', '$username','$added_by','$timestamp')";
-        mysqli_query($conn,$sql) or die(mysqli_error($conn));
-
-            // $results1 = mysqli_query($conn,"select email_id from faculty where email_id='$email'");
-            // $results2 = mysqli_query($conn,"select faculty_code from faculty where faculty_code='$faculty_code'");
-            // $results3 = mysqli_query($conn,"select employee_id from faculty where employee_id='$eid'");
-
-            // if(mysqli_num_rows($results1) > 0){
-            //     echo "<script> 
-            //     $('#email_id_error').text('*This Email ID already exists');</script>";
-            //     // echo '<span id="error_email_id" class="text-danger">This id exists</span>';
-            //     // echo "Exists_email_id";
-            // }else if(mysqli_num_rows($results2) > 0){
-            //     echo "<script> 
-            //     $('#email_faculty_code').text('*This faculty code already exists');</script>";
-            //     // echo "Exists_faculty_code";
-            // }else if(mysqli_num_rows($results3) > 0){
-            //     echo "<script> 
-            //     $('#email_employee_id').text('*This employee ID already exists');</script>";
-            //     // echo "Exists_employee_id";
-            // }
-            // else{
-            //     $sql="INSERT INTO faculty(`email_id`,`faculty_code`,`employee_id`, `fname`, `mname`, `lname`, `dept_id`, `post`, `username`,`added_by`,`timestamp`) VALUES ('$email','$faculty_code',$eid,'$names[0]','$names[1]','$names[2]','$dept_id', '$post', '$username','$added_by','$timestamp')";
-            //     mysqli_query($conn,$sql) or die(mysqli_error($conn));
-            // }
-            header("Location: ../addfaculty_internal.php");
-        
-    }
-    else if(isset($_POST['delete_internal_faculty'])){
-        $email_id=mysqli_escape_string($conn,$_POST['email_id']);
-            $sql="DELETE FROM faculty WHERE email_id='$email_id'";
-        mysqli_query($conn,$sql);
-        // header("Location: ../addcourse_ac.php");
-        exit();
-    }
-    else if(isset($_POST['update_internal_faculty'])){
-        $fname_new=mysqli_escape_string($conn,$_POST['fname_new']);
-        $mname_new=mysqli_escape_string($conn,$_POST['mname_new']);
-        $lname_new=mysqli_escape_string($conn,$_POST['lname_new']);
-        $email_id_new=mysqli_escape_string($conn,$_POST['email_id_new']);
-        $email_id_old=mysqli_escape_string($conn,$_POST['email_id_old']);
-        $faculty_code_new=mysqli_escape_string($conn,$_POST['faculty_code_new']);
-        $faculty_code_old=mysqli_escape_string($conn,$_POST['faculty_code_old']);
-        $employee_id_new=mysqli_escape_string($conn,$_POST['employee_id_new']);
-        $employee_id_old=mysqli_escape_string($conn,$_POST['employee_id_old']);
-        $dept_id=mysqli_escape_string($conn,$_POST['dept_id']);
-        $post_new=mysqli_escape_string($conn,$_POST['post_new']);
-        if($email_id_new != $email_id_old){
-=======
         $names = explode(" ", $name);
         $added_by = $_SESSION['email'];
         $sql = "SELECT username FROM login_role WHERE email_id='$email'";
@@ -151,7 +76,6 @@ if (isset($_SESSION['email']) && in_array($_SESSION['role'], $allowed_roles)) {
         $post_new = mysqli_escape_string($conn, $_POST['post_new']);
         $role_new = mysqli_escape_string($conn, $_POST['role_new']);
         if ($email_id_new != $email_id_old) {
->>>>>>> dc92e723bb853cecd634325384b23c64a55b8156
             // $query = "SELECT `email_id` FROM `faculty` WHERE `email_id`='$email_id_new'";
             $results = mysqli_query($conn, "select email_id from faculty where email_id='$email_id_new'");
             if (mysqli_num_rows($results) > 0) {
@@ -159,15 +83,9 @@ if (isset($_SESSION['email']) && in_array($_SESSION['role'], $allowed_roles)) {
                 // console.log(".$email_id_new.")
                 // $('#email_id_error').text('Sorry....This Email ID already exists');</script>";
                 echo "Exists_email_id";
-<<<<<<< HEAD
-            }else{
-                $sql="UPDATE `faculty` SET `email_id`='$email_id_new',`faculty_code`='$faculty_code_new',`employee_id`='$employee_id_new',`fname`='$fname_new',
-                `mname`='$mname_new',`lname`='$lname_new',`dept_id`='$dept_id',`post`='$post_new'
-=======
             } else {
                 $sql = "UPDATE `faculty` SET `email_id`='$email_id_new',`faculty_code`='$faculty_code_new',`employee_id`='$employee_id_new',`fname`='$fname_new',
                 `mname`='$mname_new',`lname`='$lname_new',`dept_id`='$dept_id',`post`='$post_new',`role`='$role_new'
->>>>>>> dc92e723bb853cecd634325384b23c64a55b8156
                 WHERE `email_id`='$email_id_old' AND `faculty_code`='$faculty_code_old'";
                 $sql2 = "UPDATE login_role SET email_id='$email_id_new',role='$role_new' WHERE email_id='$email_id_old'";
                 // echo"
@@ -186,15 +104,9 @@ if (isset($_SESSION['email']) && in_array($_SESSION['role'], $allowed_roles)) {
                 // console.log(".$email_id_new.")
                 // $('#email_id_error').text('Sorry....This Email ID already exists');</script>";
                 echo "Exists_faculty_code";
-<<<<<<< HEAD
-            }else{
-                $sql="UPDATE `faculty` SET `email_id`='$email_id_new',`faculty_code`='$faculty_code_new',`employee_id`='$employee_id_new',`fname`='$fname_new',
-                `mname`='$mname_new',`lname`='$lname_new',`dept_id`='$dept_id',`post`='$post_new'
-=======
             } else {
                 $sql = "UPDATE `faculty` SET `email_id`='$email_id_new',`faculty_code`='$faculty_code_new',`employee_id`='$employee_id_new',`fname`='$fname_new',
                 `mname`='$mname_new',`lname`='$lname_new',`dept_id`='$dept_id',`post`='$post_new',`role`='$role_new'
->>>>>>> dc92e723bb853cecd634325384b23c64a55b8156
                 WHERE `email_id`='$email_id_old' AND `faculty_code`='$faculty_code_old'";
                 mysqli_query($conn, $sql);
                 $sql2 = "UPDATE login_role SET email_id='$email_id_new',role='$role_new' WHERE email_id='$email_id_old'";
@@ -209,15 +121,9 @@ if (isset($_SESSION['email']) && in_array($_SESSION['role'], $allowed_roles)) {
                 // console.log(".$email_id_new.")
                 // $('#email_id_error').text('Sorry....This Email ID already exists');</script>";
                 echo "Exists_employee_id";
-<<<<<<< HEAD
-            }else{
-                $sql="UPDATE `faculty` SET `email_id`='$email_id_new',`faculty_code`='$faculty_code_new',`employee_id`='$employee_id_new',`fname`='$fname_new',
-                `mname`='$mname_new',`lname`='$lname_new',`dept_id`='$dept_id',`post`='$post_new'
-=======
             } else {
                 $sql = "UPDATE `faculty` SET `email_id`='$email_id_new',`faculty_code`='$faculty_code_new',`employee_id`='$employee_id_new',`fname`='$fname_new',
                 `mname`='$mname_new',`lname`='$lname_new',`dept_id`='$dept_id',`post`='$post_new',`role`='$role_new'
->>>>>>> dc92e723bb853cecd634325384b23c64a55b8156
                 WHERE `email_id`='$email_id_old' AND `faculty_code`='$faculty_code_old'";
                 mysqli_query($conn, $sql);
                 $sql2 = "UPDATE login_role SET email_id='$email_id_new',role='$role_new' WHERE email_id='$email_id_old'";
@@ -226,16 +132,9 @@ if (isset($_SESSION['email']) && in_array($_SESSION['role'], $allowed_roles)) {
                 // header("Location: ../addfaculty_internal.php");
                 exit();
             }
-<<<<<<< HEAD
-        }
-        else{
-            $sql="UPDATE `faculty` SET `email_id`='$email_id_new',`faculty_code`='$faculty_code_new',`employee_id`='$employee_id_new',`fname`='$fname_new',
-            `mname`='$mname_new',`lname`='$lname_new',`dept_id`='$dept_id',`post`='$post_new'
-=======
         } else {
             $sql = "UPDATE `faculty` SET `email_id`='$email_id_new',`faculty_code`='$faculty_code_new',`employee_id`='$employee_id_new',`fname`='$fname_new',
             `mname`='$mname_new',`lname`='$lname_new',`dept_id`='$dept_id',`post`='$post_new',`role`='$role_new'
->>>>>>> dc92e723bb853cecd634325384b23c64a55b8156
             WHERE `email_id`='$email_id_old' AND `faculty_code`='$faculty_code_old'";
             mysqli_query($conn, $sql);
             $sql2 = "UPDATE login_role SET email_id='$email_id_new',role='$role_new' WHERE email_id='$email_id_old'";
