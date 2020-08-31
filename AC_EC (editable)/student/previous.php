567f41aae628d1be493c6661085a3079c0a522fc
hasti-15 <hasti.ds@somaiya.edu>
2020-08-31 20:07:22 +0530

    Error resolved

--- AC_EC (editable)/student/previous.php
+++ AC_EC (editable)/student/previous.php
@@ -17,7 +17,7 @@ include_once('verify.php');
         <div class="card-header py-3">
             <div class="row align-items-center">
                 <div class="col">
-                    <h4 class="font-weight-bold text-primary mb-0">Student Records</h4>
+                    <h4 class="font-weight-bold text-primary mb-0 pb-2">Previous Course Records</h4>
                 </div>
             </div>
 
@@ -29,41 +29,85 @@ include_once('verify.php');
                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                             <thead>
                                 <tr>
+                                    <th>Course Type </th>
                                     <th>Course Name</th>
                                     <th>Course ID</th>
                                     <th>Year</th>
                                     <th>Semester</th>
-                                    <th>Floating Departments</th>
-                                    <th>About</th>
+                                    <th>Marks </th>
+                                    <th>Completion Status</th>
+                                    <th>Attendance</th>
                                 </tr>
                             </thead>
                             <tfoot>
                                 <tr>
+                                    <th>Course Type </th>
                                     <th>Course Name</th>
                                     <th>Course ID</th>
                                     <th>Year</th>
                                     <th>Semester</th>
-                                    <th>Floating Departments</th>
-                                    <th>About</th>
+                                    <th>Marks </th>
+                                    <th>Completion Status</th>
+                                    <th>Attendance</th>
+                                    
+                                    
                                 </tr>
                             </tfoot>
                             <tbody>
                             <?php 
-                                $sql="SELECT sal.cid,sal.sem,sal.year,acl.cname,(SELECT GROUP_CONCAT(dept_name SEPARATOR ', ') FROM audit_course_floating_dept_log afd 
-         INNER JOIN department d2 ON afd.dept_id=d2.dept_id WHERE acl.cid=afd.cid AND acl.sem=afd.sem AND acl.year=afd.year
-         GROUP BY 'all') as dept_name,f.fname,f.mname,f.lname,sal.complete_status,sal.student_attendence FROM student_audit_log AS sal INNER Join audit_course_log AS acl on sal.cid=acl.cid AND sal.year=acl.year AND sal.sem=acl.sem INNER Join faculty_audit_log AS fal on fal.cid=acl.cid AND fal.sem=acl.sem AND fal.year=acl.year INNER Join  faculty AS f on f.email_id=fal.email_id WHERE sal.email_id='{$_SESSION['email']}'";
-                               if ($result = mysqli_query($conn,$sql)){
+
+                                $sql= "SELECT scl.cid, scl.sem,scl.course_type_id,scl.year,cl.cname,course_types.name,course_types.is_gradable from student_course_alloted_log as scl inner join course_log as cl on scl.cid=cl.cid inner join course_types on cl.course_type_id=course_types.id where scl.email_id='{$_SESSION['email']}'";
+                                $result=mysqli_query($conn,$sql);
                                 $count=0;
-                                 while($row = mysqli_fetch_array($result)) 
-                                 {
-                            ?>
-                                <tr>
-                                    <td><?php echo $row['cname'];?></td>
-                                    <td><?php echo $row['cid'];?></td>
-                                    <td><?php echo $row['year'];?></td>
-                                    <td><?php echo $row['sem'];?></td>
-                                    <td><?php echo $row['dept_name'];?></td>
-                                    <td>
+
+                                while($row=mysqli_fetch_array($result)){
+
+                                    $sem=$row['sem']; 
+                                    $year=$row['year'];
+                                    $cid= $row['cid'];
+                                    $course_type_id=$row['course_type_id'];
+
+                                    ?>
+
+                                    <tr>
+                                        <td><?php echo $row['name']; ?> </td>
+                                        <td><?php echo $row['cname']; ?> </td>
+                                        <td><?php echo $row['cid']; ?> </td>
+                                        <td><?php echo $row['year']; ?></td>
+                                        <td><?php echo $row['sem']; ?></td>
+
+                                        <?php if($row['is_gradable']==1){
+
+                                            $sql1="SELECT scgl.marks,scgl.complete_status,scgl.student_attendance from student_courses_grade_log as scgl,student_course_alloted_log as scl where scgl.email_id='{$_SESSION['email']}' AND scgl.cid='$cid' and scgl.course_type_id='$course_type_id' AND scgl.sem='$sem' and scgl.year='$year'";
+                                            $result1= mysqli_query($conn,$sql1);
+                                            while($row1=mysqli_fetch_array($result1))
+                                            {  ?>
+                                                
+                                                <td> <?php echo $row1['marks']; ?> </td>
+                                                <td> <?php echo $row1['complete_status']; ?> </td>
+                                                <td> <?php echo $row1['student_attendance']; ?> </td>
+                                                </tr>
+                                            
+                                            <?php} 
+
+                                        }
+
+                                        else{
+                                                $sql2="SELECT scngl.complete_status,scngl.student_attendance from student_courses_nongrade_log as scngl,student_course_alloted_log as scl where scngl.email_id='{$_SESSION['email']}' AND scngl.cid='$cid' and scngl.course_type_id='$course_type_id' AND scngl.sem='$sem' and scngl.year='$year'";
+                                                $result2= mysqli_query($conn,$sql1);
+                                                while($row2=mysqli_fetch_array($result2)){
+                                                    ?> 
+                                                    <td> <?php echo 'NA' ?> </td>
+                                                    <td> <?php echo $row2['complete_status']; ?> </td>
+                                                    <td> <?php echo $row2['student_attendance']; ?> </td>
+                                                    </tr>
+
+                                               <?php }
+                                            }
+                                 ?>
+
+
+                             
 
                                         <!-- Button trigger modal -->
                                         <button type="button" class="btn btn-primary icon-btn" data-toggle="modal" data-target="#exampleModalCenter<?php echo $count;?>">
@@ -142,7 +186,7 @@ include_once('verify.php');
                                         </div>
                                     </td>
                                 </tr>
-                            <?php } }?>
+                            <?php } ?>
                             </tbody>
                         </table>
                     </div>
